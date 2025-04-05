3c448dfd31a45eba554782d2183fab2e26da86d8
Arora962 <157892517+Arora962@users.noreply.github.com>
2025-04-05 13:05:51 +0530

    Initial Commit

---
+++ iPlateTests/iPlateTests.swift
@@ -0,0 +1,17 @@
+//
+//  iPlateTests.swift
+//  iPlateTests
+//
+//  Created by Kriti Arora on 05/04/25.
+//
+
+import Testing
+@testable import iPlate
+
+struct iPlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
